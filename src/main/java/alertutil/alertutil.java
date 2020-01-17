e0e2ffc0873b955609eef9022759a98009242474
Chengwei Zhang <1535909927@qq.com>
2020-01-17 11:26:52 +0800

    Add a hashmap to save alerts

--- src/main/java/alertutil/alertutil.java
+++ src/main/java/alertutil/alertutil.java
@@ -1,20 +1,94 @@
 package alertutil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class AlertUtil {
-    private AtomicInteger val = new AtomicInteger(0);
-
     private static class Holder {
         private static AlertUtil instance = new AlertUtil();
+    }
+
+    public static class Rule {
+        private int expire_time;
+
+        public Rule(int expire_time) {
+            this.expire_time = expire_time;
+        }
+
+        public int getExpire_time() {
+            return expire_time;
+        }
+
+        public void setExpire_time(int expire_time) {
+            this.expire_time = expire_time;
+        }
+    }
+
+    private static Map<String, Rule> rules;
+
+    private AlertUtil() {
+        rules = new HashMap<String, Rule>();
+        rules.put("default", new Rule(5000));
     }
 
     public static AlertUtil getInstance() {
         return Holder.instance;
     }
 
-    public void addone() {
-        System.out.println(this.val.addAndGet(1));
+    public static class Alert {
+        private Rule rule;
+        private String content;
+        private long timestamp;
+
+        public Alert(Rule rule, String content) {
+            this.rule = rule;
+            this.content = content;
+            this.timestamp = System.currentTimeMillis();
+        }
+
+        public Alert(String rule_name, String content) {
+            this.rule = rules.get(rule_name);
+            this.content = content;
+            this.timestamp = System.currentTimeMillis();
+        }
+
+        public Rule getRule() {
+            return rule;
+        }
+
+        public void setRule(Rule rule) {
+            this.rule = rule;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
+
+    private static Map<String, Alert> alerts = new ConcurrentHashMap<String, Alert>();
+
+    public void add_alert(Alert alert) {
+        String key = String.valueOf(alert.content.hashCode());
+        if (alerts.get(key) == null) {
+            alerts.put(key, alert);
+            System.out.println("In thread " + alert.content + " key= " + key + " " + alert.timestamp);
+        }
+    }
+
+    public void print_alerts() {
+        System.out.println("last size " + alerts.size() + ". Contents: ");
+        for (int i = 0; i < 5; i ++) {
+            Alert a = alerts.get(String.valueOf(String.valueOf(i).hashCode()));
+            System.out.println(a.content + " " + a.timestamp);
+        }
     }
 
 }
--- src/main/java/alertutil/test.java
+++ src/main/java/alertutil/test.java
@@ -1,20 +1,41 @@
 package alertutil;
 
+import java.util.concurrent.TimeUnit;
+
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 5; i ++) {
-            Thread t = new alert_invoker();
+            Thread t = new alert_invoker(i);
             System.out.println("Starting thread " + i + "... ");
             t.start();
+        }
+        TimeUnit.SECONDS.sleep(1);
+        Thread q = new alert_invoker(3);
+        System.out.println("Starting thread with identical content... ");
+        q.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        Thread t_out = new alert_out();
+        t_out.start();
+    }
+
+    static class alert_out extends Thread {
+        @Override
+        public void run() {
+            AlertUtil a = AlertUtil.getInstance();
+            a.print_alerts();
         }
     }
 
     static class alert_invoker extends Thread {
-        private alert_invoker() {};
+        private int id;
+        private alert_invoker(int id) {
+            this.id = id;
+        };
         @Override
         public void run() {
             AlertUtil a = AlertUtil.getInstance();
-            a.addone();
+            a.add_alert(new AlertUtil.Alert("default", String.valueOf(this.id)));
         }
     }
 }
